e7c7551475ff9f7aa019dee2ab436d56c67ef93e
Damo-1981 <damian.king@weaveability.com>
2020-06-24 16:50:03 +0000

    Resource Group Filter + onClick Handler

--- webui5/resources/webapp/controller/Calendar.controller.js
+++ webui5/resources/webapp/controller/Calendar.controller.js
@@ -1,19 +1,49 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
+		"sap/ui/core/mvc/Controller",
+		'sap/m/MessageBox'
+	],
+	function (Controller, MessageBox) {
+		"use strict";
 
-	return Controller.extend("calendar-app.webui5.controller.Calendar", {
-		onInit: function () {
-			// Initiate the OData Model
-			var oParams = {};
-			oParams.json = true;
-			oParams.useBatch = true;
-			var oModel = new sap.ui.model.odata.v2.ODataModel("/xsodata/Calendar.xsodata", oParams);
-			
-			// Load the table
-			var oCalendar = this.getView().byId("myCal");
-			oCalendar.setModel(oModel);
-		}
+		return Controller.extend("calendar-app.webui5.controller.Calendar", {
+			onInit: function () {
+				// Get query Parameter
+				var resGroup = jQuery.sap.getUriParameters().get("rg");
+				if(!resGroup) {resGroup = "0000000001";}
+				
+				// Create filter
+				var resGroupFilter = [ new sap.ui.model.Filter("EMPRESOURCEGRP","EQ", resGroup) ];
+				
+				// Grab the OData Model
+				var oModel = this.getOwnerComponent().getModel();
+
+				// Load the Calendar
+				var oCalendar = this.getView().byId("myCal");
+				oCalendar.setModel(oModel);
+				
+				// Apply the filter
+				var oBinding = oCalendar.getBinding("rows");
+    			oBinding.filter(resGroupFilter);
+			},
+
+			handleAppointmentSelect: function (oEvent) {
+				var oAppointment = oEvent.getParameter("appointment");
+				if (oAppointment) {
+					if(oAppointment.getSelected()) {
+						var oContext = oAppointment.getBindingContext();
+						if (oContext) {
+							MessageBox.information("WBS Code: " + oContext.getProperty("BKWBS") + "\n"
+								+ "WBS Desc: " + oContext.getProperty("BKWBSDESC") + "\n"
+								+ "Customer: " + oContext.getProperty("BKCUSTOMER") + "\n"
+								+ "Location: " + oContext.getProperty("BKCUSTOMERLOCATION"));
+						}
+					}
+				}
+			},
+
+			handleSelectionFinish: function (oEvent) {
+				var aSelectedKeys = oEvent.getSource().getSelectedKeys();
+				this.byId("myCal").setBuiltInViews(aSelectedKeys);
+			}
+		});
 	});
-});
