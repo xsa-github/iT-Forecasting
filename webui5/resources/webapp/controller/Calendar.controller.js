b9fac80fe1913e8133f89ce5e85d400eb3944b45
Damo-1981 <damian.king@weaveability.com>
2020-06-30 10:25:35 +0000

    Filters added

--- webui5/resources/webapp/controller/Calendar.controller.js
+++ webui5/resources/webapp/controller/Calendar.controller.js
@@ -1,49 +1,81 @@
 sap.ui.define([
 		"sap/ui/core/mvc/Controller",
-		'sap/m/MessageBox'
+		"sap/m/MessageBox"
 	],
 	function (Controller, MessageBox) {
 		"use strict";
 
 		return Controller.extend("calendar-app.webui5.controller.Calendar", {
 			onInit: function () {
-				// Get query Parameter
-				var resGroup = jQuery.sap.getUriParameters().get("rg");
-				if(!resGroup) {resGroup = "0000000001";}
-				
-				// Create filter
-				var resGroupFilter = [ new sap.ui.model.Filter("EMPRESOURCEGRP","EQ", resGroup) ];
-				
 				// Grab the OData Model
 				var oModel = this.getOwnerComponent().getModel();
+				
+				// Grab the JSON Model
+				var fModel = this.getOwnerComponent().getModel("filterData");
+				
+				// Get query Parameter and set as property of the JSON model
+				var resGroup = jQuery.sap.getUriParameters().get("rg");
+				if (!resGroup) {
+					resGroup = "0000000001";
+				}
+				
+				// Bind the Filters
+				var oCombo1 = this.getView().byId("cb1");
+				oCombo1.setModel(fModel);
+				oCombo1.setSelectedKey(resGroup);
+				
+				var oCombo2 = this.getView().byId("cb2");
+				oCombo2.setModel(fModel);
 
 				// Load the Calendar
 				var oCalendar = this.getView().byId("myCal");
 				oCalendar.setModel(oModel);
-				
-				// Apply the filter
-				var oBinding = oCalendar.getBinding("rows");
-    			oBinding.filter(resGroupFilter);
+				oCalendar.setBuiltInViews([sap.m.PlanningCalendarBuiltInView.Day,sap.m.PlanningCalendarBuiltInView.Week,sap.m.PlanningCalendarBuiltInView.OneMonth]);
+			},
+			
+			onBeforeRendering: function () {
+				this.filterCalendar();
 			},
 
 			handleAppointmentSelect: function (oEvent) {
 				var oAppointment = oEvent.getParameter("appointment");
 				if (oAppointment) {
-					if(oAppointment.getSelected()) {
+					if (oAppointment.getSelected()) {
 						var oContext = oAppointment.getBindingContext();
 						if (oContext) {
-							MessageBox.information("WBS Code: " + oContext.getProperty("BKWBS") + "\n"
-								+ "WBS Desc: " + oContext.getProperty("BKWBSDESC") + "\n"
-								+ "Customer: " + oContext.getProperty("BKCUSTOMER") + "\n"
-								+ "Location: " + oContext.getProperty("BKCUSTOMERLOCATION"));
+							MessageBox.information("WBS Code: " + oContext.getProperty("BKWBS") + "\n" + "WBS Desc: " + oContext.getProperty("BKWBSDESC") +
+								"\n" + "Customer: " + oContext.getProperty("BKCUSTOMER") + "\n" + "Location: " + oContext.getProperty("BKCUSTOMERLOCATION"));
 						}
 					}
 				}
 			},
 
-			handleSelectionFinish: function (oEvent) {
-				var aSelectedKeys = oEvent.getSource().getSelectedKeys();
-				this.byId("myCal").setBuiltInViews(aSelectedKeys);
+			onSearch: function () {
+				this.filterCalendar();
+			},
+			
+			/*
+			Apply filters to the Calendar when the selection changes.
+			 */
+			filterCalendar: function () {
+
+				var oCombo1 = this.getView().byId("cb1");
+				var resGroupKey = oCombo1.getSelectedKey();
+				if (resGroupKey) {
+					var resGroupFilter = [new sap.ui.model.Filter("EMPRESOURCEGRP", "EQ", resGroupKey)];
+					var oCalendar = this.getView().byId("myCal");
+					var oBinding = oCalendar.getBinding("rows");
+					oBinding.filter(resGroupFilter);
+				}
+				
+				var oCombo2 = this.getView().byId("cb2");
+				var bkTypeKey = oCombo2.getSelectedKey();
+				if (bkTypeKey) {
+					var bkTypeFilter = [new sap.ui.model.Filter("BKTYPE", "EQ", bkTypeKey)];
+					var oCalendarRow = this.getView().byId("myCalRow");
+					var oBindingApp = oCalendarRow.getBinding("appointments");
+					oBindingApp.filter(bkTypeFilter);
+				}
 			}
 		});
 	});
